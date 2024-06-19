f5c6884e0ab316f583f10a47e5ca75b9ab6369fc
samvidav <samvida.venkatesh@gmail.com>
2024-06-19 13:15:37 +0100

    Remove rsid adding in lead SNPs
    Because we've already added rsids to the whole file

--- 3_meta_analysis/hormones/get_lead_snps/get_lead_snps_distance_pruning.sh
+++ 3_meta_analysis/hormones/get_lead_snps/get_lead_snps_distance_pruning.sh
@@ -25,31 +25,11 @@ cd /well/lindgren/samvida/hormones_infertility/meta_results_230613
 
 mkdir lead_snps
 
-awk -F '\t' 'BEGIN {OFS = FS} {if ($11 <= 5E-08) print}' \
-filtered/${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_filtered.txt \
-> lead_snps/gws_variants_${HORMONE}_${SEX_STRATA}_${ANC_GROUP}.txt
+awk -F '\t' 'BEGIN {OFS = FS} {if ($12 <= 5E-08) print}' \
+add_rsids/${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_filtered.txt \
+> lead_snps/gws_variants_${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_with_rsids.txt
 
 cd lead_snps
-# Sort by chr:position to add rsid
-# Replace "chr23" with "chrX" in order to get the rsids
-sed -i 's/chr23/chrX/g' gws_variants_${HORMONE}_${SEX_STRATA}_${ANC_GROUP}.txt
-
-join -t $'\t' -1 1 -2 6 -o 1.1,2.5,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10,1.11,1.12,1.13,2.3,2.4 \
-<(sort -k1 gws_variants_${HORMONE}_${SEX_STRATA}_${ANC_GROUP}.txt) \
-/well/lindgren/samvida/Resources/hg38/grch38.withchrpos.all.rsid.txt \
-> ${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_tmp.txt
-
-# Remove rows where the map alleles don't match the sumstat alleles
-awk 'BEGIN { FS = OFS = "\t" } { split($16, values, ","); for (i in values) if ($6 == $15 && $7 == values[i] || $7 == $15 && $6 == values[i]) { print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14; break } }' \
-${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_tmp.txt \
-> gws_variants_${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_with_rsids.txt
-
-# Add header column
-sed -i '1s/^/ID\tRSID\tCHROM\tGENPOS\tMAF\tAllele1\tAllele2\tFreq1\tFreqSE\tBETA\tSE\tPVALUE\tDirection\tHetPVal\n/' \
-gws_variants_${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_with_rsids.txt
-
-rm gws_variants_${HORMONE}_${SEX_STRATA}_${ANC_GROUP}.txt
-rm ${HORMONE}_${SEX_STRATA}_${ANC_GROUP}_tmp.txt
 
 # Also create a subset of GWS variants that are present in the 1000 Genomes project (higher confidence + for classification)
 # Load PLINK module
@@ -57,7 +37,7 @@ module load PLINK/2.00a2.3_x86_64
 
 mkdir plink_log
 # Read all GWS SNPs and create list of variants to check for
-# Remvoe any SNPs on X chromosome because we can't classify these with LD anyway
+# Remove any SNPs on X chromosome because we can't classify these with LD anyway
 awk '$3 != 23 {print $3":"$4":"$6":"$7}' gws_variants_*_with_rsids.txt | sort | uniq > plink_log/all_gws_varids.txt
 
 # Extract bim file for variants of interest 
