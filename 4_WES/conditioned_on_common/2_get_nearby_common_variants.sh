e85ecfda6751aa2433664706cc006300a56c9acc
samvidav <samvida.venkatesh@gmail.com>
2024-06-21 12:22:25 +0100

    Rewrite to produce plink files

--- 4_WES/conditioned_on_common/2_get_nearby_common_variants.sh
+++ 4_WES/conditioned_on_common/2_get_nearby_common_variants.sh
@@ -2,6 +2,8 @@
 
 cd /well/lindgren/samvida/hormones_infertility/exome_seq_results/conditioned_on_common
 
+echo -e "phenotype\trare_variant\tcommon_snps" > snplist_to_condition.txt
+
 declare -a HORMONES=("Oestradiol_F" "Testosterone_F" "Testosterone_M")
 
 for hr in "${HORMONES[@]}"; do
@@ -21,11 +23,20 @@ for hr in "${HORMONES[@]}"; do
 		awk -v pos_match="$pos_match" -v chr_match="$chr_match" '
 			$3 == chr_match && $4 >= pos_match - 500000 && $4 <= pos_match + 500000
 		' "/well/lindgren/samvida/hormones_infertility/meta_results_230613/lead_snps/all_lead_snp_sumstats_${hr}_EUR_with_rsids.txt" \
-		>> nearby_common_variants/${hr}_nearby_common_variant_sumstats.txt
+		> nearby_common_variants/tmp.txt
+		
+		cat nearby_common_variants/tmp.txt >> nearby_common_variants/${hr}_nearby_common_variant_sumstats.txt
+
+		# Print SNP list per rare variant 
+		rare_var=$(echo "$variant_line" | awk '{print $3}')
+		common_snps=$(awk '{print $2}' nearby_common_variants/tmp.txt | paste -sd, -)
+		echo -e "${hr}\t${rare_var}\t${common_snps}" >> snplist_to_condition.txt
+
+		rm nearby_common_variants/tmp.txt
 	done  
 done
 
-# Concatenate all the unique SNPs into a single file
+# Concatenate all the unique SNPs into a single file for plink
 for fname in nearby_common_variants/*_nearby_common_variant_sumstats.txt; do
 	# Extract the RSID, chromosome, and hg38 position
 	tail -n +2 "$fname" | awk '{print $2 "\t" $3 "\t" $4}' \
--- 4_WES/conditioned_on_common/3_get_dosages_common_variants_chrX.sh
+++ 4_WES/conditioned_on_common/3_get_dosages_common_variants_chrX.sh
@@ -3,8 +3,7 @@
 #SBATCH -A lindgren.prj
 #SBATCH -p short
 #SBATCH -c 5
-#SBATCH --array 2,4,6,7,11,12,16,17,19,20
-#SBATCH -J extract_genotypes
+#SBATCH -J extract_genotypes_chrX
 
 echo "########################################################"
 echo "Slurm Job ID: $SLURM_JOB_ID" 
@@ -16,23 +15,21 @@ echo "##########################################################"
 
 module load PLINK/2.00a2.3_x86_64
 
-CHR_GET=${SLURM_ARRAY_TASK_ID}
-
 cd /well/lindgren/samvida/hormones_infertility/exome_seq_results/conditioned_on_common
 
-awk -v chr="${CHR_GET}" '$2 == chr {print $1}' nearby_common_variants/unique_rsids_to_get.txt \
-> tmp_chr${CHR_GET}_rsids.txt
+awk -v '$2 == 23 {print $1}' nearby_common_variants/unique_rsids_to_get.txt \
+> tmp_chrX_rsids.txt
 
 plink2 \
---bgen /well/lindgren-ukbb/projects/ukbb-11867/DATA/IMPUTATION/ukb_imp_chr${CHR_GET}_v3.bgen \
---sample /well/lindgren-ukbb/projects/ukbb-11867/DATA/SAMPLE_FAM/ukb11867_imp_chr1_v3_s487395.sample \
---extract tmp_chr${CHR_GET}_rsids.txt \
+--bgen /well/lindgren-ukbb/projects/ukbb-11867/DATA/IMPUTATION/ukb_imp_chrX_v3.bgen \
+--sample /well/lindgren-ukbb/projects/ukbb-11867/DATA/SAMPLE_FAM/ukb11867_imp_chrX_v3_s486743.sample \
+--extract tmp_chrX_rsids.txt \
 --make-pgen \
---out plink_files/chr${CHR_GET}_rsids \
+--out plink_files/chrX_rsids \
 --memory 15000 \
 --threads 5 
 
-rm tmp_chr${CHR_GET}_rsids.txt
+rm tmp_chrX_rsids.txt
 
 echo "###########################################################"
 echo "Finished at: "`date`
